1e7e5218df64ca58c10aeedad10e666f12c31291
alrudolph <alex3r@me.com>
2021-05-17 15:21:00 -0700

    update ghpages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-;(async () => {
-  console.log("Plotting map...")
-
-  const CA_MAP_URL =
-    "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/us-states/CA-06-california-counties.json"
-
-  const DATA_URL =
-    "https://data.chhs.ca.gov/api/3/action/datastore_search?resource_id=046cdd2b-31e5-4d34-9ed3-b48cdbc4be7a&q=2021-01&limit=10000"
-})()
